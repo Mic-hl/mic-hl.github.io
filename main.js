91769ed0a3d2b9f2fd14bba733ee44113568d4db
Mic-hl <73788645+Mic-hl@users.noreply.github.com>
2020-11-05 13:28:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 var imgs = document.getElementById("playground_imgs");
 imgs.onclick = function () {
     var mySrc = imgs.getAttribute("src");
-    if(mySrc === "tree01.jpg") {
-        imgs.setAttribute("src", "tree02.jpg");
-    } else if (mySrc === "tree02.jpg") {
-        imgs.setAttribute("src", "tree03.jpeg");
+    if(mySrc === "/images/tree01.jpg") {
+        imgs.setAttribute("src", "/images/tree02.jpg");
+    } else if (mySrc === "/images/tree02.jpg") {
+        imgs.setAttribute("src", "/images/tree03.jpeg");
     } else {
-        imgs.setAttribute("src", "tree01.jpg");
+        imgs.setAttribute("src", "/images/tree01.jpg");
     }
 }
