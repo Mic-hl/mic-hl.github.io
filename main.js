84f1adcd9174ea1f13fdbf25a85367250e6adc9f
Mic-hl <73788645+Mic-hl@users.noreply.github.com>
2020-11-05 12:18:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 
 var imgs = document.getElementById("playground_imgs");
 imgs.onclick = function () {
-    var mySrc = myImageA.getAttribute("src");
+    var mySrc = imgs.getAttribute("src");
     if(mySrc === "https://www.thetreecenter.com/wp-content/uploads/pin-oak-2.jpg") {
-        myImageA.setAttribute("src", "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/a-tree-in-the-forest-vertical-lisa-wooten.jpg");
+        imgs.setAttribute("src", "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/a-tree-in-the-forest-vertical-lisa-wooten.jpg");
     } else if (mySrc === "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/a-tree-in-the-forest-vertical-lisa-wooten.jpg") {
-        myImageA.setAttribute("src", "https://www.freechristmaswallpapers.net/web/wallpapers/Snowy-Christmas-Tree_640x960.jpeg");
+        imgs.setAttribute("src", "https://www.freechristmaswallpapers.net/web/wallpapers/Snowy-Christmas-Tree_640x960.jpeg");
     } else {
-        myImageA.setAttribute("src", "https://www.thetreecenter.com/wp-content/uploads/pin-oak-2.jpg");
+        imgs.setAttribute("src", "https://www.thetreecenter.com/wp-content/uploads/pin-oak-2.jpg");
     }
 }
