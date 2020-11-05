2e6b8d5031b5ca3055e921cf72e5b22fb6c933e9
Mic-hl <73788645+Mic-hl@users.noreply.github.com>
2020-11-05 13:23:28 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 var imgs = document.getElementById("playground_imgs");
 imgs.onclick = function () {
     var mySrc = imgs.getAttribute("src");
-    if(mySrc === "https://www.thetreecenter.com/wp-content/uploads/pin-oak-2.jpg") {
-        imgs.setAttribute("src", "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/a-tree-in-the-forest-vertical-lisa-wooten.jpg");
-    } else if (mySrc === "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/a-tree-in-the-forest-vertical-lisa-wooten.jpg") {
-        imgs.setAttribute("src", "https://www.freechristmaswallpapers.net/web/wallpapers/Snowy-Christmas-Tree_640x960.jpeg");
+    if(mySrc === "tree01.jpg") {
+        imgs.setAttribute("src", "tree02.jpg");
+    } else if (mySrc === "tree02.jpg") {
+        imgs.setAttribute("src", "tree03.jpeg");
     } else {
-        imgs.setAttribute("src", "https://www.thetreecenter.com/wp-content/uploads/pin-oak-2.jpg");
+        imgs.setAttribute("src", "tree01.jpg");
     }
 }
