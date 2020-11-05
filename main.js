12a3deb9e5bcabee35affa96252cbf3202fe9dd6
Mic-hl <73788645+Mic-hl@users.noreply.github.com>
2020-11-05 12:04:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // images
 
-var myImageA = document.getElementById("playground_imgs");
-myImageA.onclick = function () {
+var imgs = document.getElementById("playground_imgs");
+imgs.onclick = function () {
     var mySrc = myImageA.getAttribute("src");
     if(mySrc === "https://www.thetreecenter.com/wp-content/uploads/pin-oak-2.jpg") {
         myImageA.setAttribute("src", "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/a-tree-in-the-forest-vertical-lisa-wooten.jpg");
